d58a10e4d0c83466d81affa4be0fdde4f3b2dbfc
ihm-mvp <supermilkbaba@gmail.com>
2026-07-07 07:45:53 +0800

    fix agent

--- lib/listing/importListing.ts
+++ lib/listing/importListing.ts
@@ -30,16 +30,27 @@ async function parseHarcourtsGold(
   // Fetch HTML
   // =====================================
 
-  const response =
-    await fetch(
-      url,
-      {
-        headers: {
-          "User-Agent":
-            "Mozilla/5.0",
-        },
-      }
-    )
+const response =
+  await fetch(
+    url,
+    {
+      headers: {
+
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+
+        "Accept":
+          "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+
+        "Accept-Language":
+          "en-NZ,en;q=0.9",
+
+        "Referer":
+          "https://www.google.com/",
+
+      },
+    }
+  )
 
 if (
   !response.ok
