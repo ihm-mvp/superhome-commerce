c12123c7ac2703c29878cf96ef64a44feee33f44
ihm-mvp <supermilkbaba@gmail.com>
2026-07-08 11:14:21 +0800

    add why this property

--- app/listing/[slug]/page.tsx
+++ app/listing/[slug]/page.tsx
@@ -7,6 +7,7 @@ import { supabase } from "@/lib/supabase"
 import Hero from "./components/Hero"
 import QuickFacts from "./components/QuickFacts"
 import EventCard from "./components/EventCard"
+import WhyThisProperty from "./components/WhyThisProperty"
 import ContactCard from "./components/ContactCard"
 
 type Props = {
@@ -80,6 +81,10 @@ export default async function ListingPage({
       <EventCard
         listing={listing}
         event={events}
+      />
+
+      <WhyThisProperty
+        listing={listing}
       />
 
       <ContactCard
