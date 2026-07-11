2c24062ea09c2a43cf5cc8feffb0386a0770608f
ihm-mvp <supermilkbaba@gmail.com>
2026-07-11 22:04:11 +0800

    FIX IMPORT LISTING

---
+++ lib/listing/compareListing.ts
@@ -0,0 +1,141 @@
+// lib/listing/compareListing.ts
+
+import {
+
+  listingComparePool,
+
+} from "./comparePool"
+
+export function compareListing(
+
+  existingListing: any,
+
+  existingOpenHomes: any[],
+
+  parsedListing: any,
+
+) {
+
+  const listingChangedFields: string[] = []
+
+  // -------------------------
+  // Listing Compare
+  // -------------------------
+
+  for (
+
+    const field of
+
+    listingComparePool
+
+  ) {
+
+    if (
+
+      existingListing[field] !==
+
+      parsedListing[field]
+
+    ) {
+
+      listingChangedFields.push(
+
+        field
+
+      )
+
+    }
+
+  }
+
+  // -------------------------
+  // Open Home Compare
+  // -------------------------
+
+  const existingOpenHomeJson =
+
+    JSON.stringify(
+
+      [...existingOpenHomes]
+
+        .map(
+
+          home => ({
+
+            openhome_date:
+
+              home.openhome_date,
+
+            start_time:
+
+              home.start_time,
+
+            end_time:
+
+              home.end_time,
+
+          })
+
+        )
+
+        .sort(
+
+          (a, b) =>
+
+            `${a.openhome_date}${a.start_time}`
+
+              .localeCompare(
+
+                `${b.openhome_date}${b.start_time}`
+
+              )
+
+        )
+
+    )
+
+  const parsedOpenHomeJson =
+
+    JSON.stringify(
+
+      [...(
+
+        parsedListing.openHomes || []
+
+      )]
+
+        .sort(
+
+          (a, b) =>
+
+            `${a.openhome_date}${a.start_time}`
+
+              .localeCompare(
+
+                `${b.openhome_date}${b.start_time}`
+
+              )
+
+        )
+
+    )
+
+  const openHomeChanged =
+
+    existingOpenHomeJson !==
+
+    parsedOpenHomeJson
+
+  return {
+
+    listingChanged:
+
+      listingChangedFields.length > 0,
+
+    listingChangedFields,
+
+    openHomeChanged,
+
+  }
+
+}
---
+++ lib/listing/comparePool.ts
@@ -0,0 +1,33 @@
+// lib/listing/comparePool.ts
+
+export const listingComparePool = [
+
+  "price_display",
+
+  "listing_status",
+
+  "description",
+
+  "bedrooms",
+
+  "bathrooms",
+
+  "garage",
+
+  "land_area",
+
+  "floor_area",
+
+  "photos",
+
+  "floorplan_image",
+
+  "video_url",
+
+] as const
+
+export const openHomeComparePool = [
+
+  "openHomes",
+
+] as const
